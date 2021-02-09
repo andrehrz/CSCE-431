6cc0137eb550df07b11051da6263446eb4e7503d
Rahul Shah <rahulshah512@gmail.com>
2021-02-08 19:24:59 -0600

    Git-ignore database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 2021_02_09_002739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "Title"
-    t.string "Author"
-    t.string "Genre"
-    t.integer "Price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.date "Published"
-  end
-
   create_table "equipment", force: :cascade do |t|
     t.string "name"
     t.string "description"
