164432c1b5c036b85320be83c85ec9f441c6c7b8
Rahul Shah <rahulshah512@gmail.com>
2021-02-13 15:38:34 -0600

    implemented create reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,9 +11,23 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new;
   end
 
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      redirect_to(reservations_path) #, notice:"#{@book.Title} Was Added !")
+    else
+      render('new')
+    end
+  end
+
   def edit
   end
 
   def delete
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:event_description, :checkout_date, :checkin_date)
   end
 end
