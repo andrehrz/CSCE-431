37f82194e0f013d90c2295aebf4afa33d78d10b0
Rahul Shah <rahulshah512@gmail.com>
2021-04-10 13:01:20 -0500

    Finished adding  Dark Mode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_203842) do
+ActiveRecord::Schema.define(version: 2021_03_16_195523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2021_02_13_203842) do
     t.boolean "is_admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
