eae6316783b7818ecf4e1bc04591007199c0f084
ilianaagui99@gmail.com <ilianaagui99@gmail.com>
2021-04-10 12:23:10 -0500

    added a violation count feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name phone_number secondary_contact])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone_number secondary_contact])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone_number secondary_contact violation_counter])
   end
 end
